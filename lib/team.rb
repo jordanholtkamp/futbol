74ce555f652ea41af8ef7b427dde7dcf9d033c35
William Meighan <williamdmeighan@gmail.com>
2019-10-29 07:31:50 -0600

    Add helper methods; average_goals_per_game and average_goals_allowed_per_game

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,6 @@
 class Team
   attr_reader :team_id, :franchiseId, :teamName, :abbreviation,
-  :Stadium, :link
+  :Stadium, :link, :team_games, :opponent_games
 
   def initialize(teams_info)
     @team_id = teams_info[:team_id].to_i
@@ -9,5 +9,23 @@ class Team
     @abbreviation = teams_info[:abbreviation]
     @Stadium = teams_info[:Stadium]
     @link = teams_info[:link]
+    @team_games = team_games
+    @opponent_games = opponent_games
   end
+
+  def average_goals_per_game
+    goal = @team_games.sum do |game|
+      game.goals
+    end
+      (goal / @team_games.length.to_f).round(2)
+  end
+
+  def average_goals_allowed_per_game
+    goal = @opponent_games.sum do |game|
+      game.goals
+    end
+    (goal / @opponent_games.length.to_f).round(2)
+  end
+
+
 end
